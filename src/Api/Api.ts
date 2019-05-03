687bdc82071f60e70767b6ee96ed7fdec2708e03
dherv <damien.herv@gmail.com>
2019-05-03 20:33:32 +0900

    develop - Update Api with dev and prod url

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -2,7 +2,9 @@
 // need to ignore the file for test coverage. class using static functions is not covered.
 export default class Api {
   static prefix =
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:8000/api/";
+    process.env.NODE_ENV === "production"
+      ? "http://13.231.85.189/api/"
+      : "http://localhost:8000/api/";
 
   static get(url: string): Promise<any> {
     return fetch(`${this.prefix}${url}`, {
