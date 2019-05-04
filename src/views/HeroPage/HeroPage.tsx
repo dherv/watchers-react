e14c2618e18572a980261a61766efab11b485d32
dherv <damien.herv@gmail.com>
2019-05-04 13:33:49 +0900

    feature(paginate) - Update HeroPage with Paginate

--- src/views/HeroPage/HeroPage.tsx
+++ src/views/HeroPage/HeroPage.tsx
@@ -2,21 +2,29 @@ import React, { useState, useEffect, SFC, Fragment } from "react";
 import Sort from "../../components/Sort/Sort";
 import MediaGrid from "../../components/MediaGrid/MediaGrid";
 import Api from "../../Api/Api";
-import { IHistory, ILocation } from "../../types/interfaces";
+import { IHistory, ILocation, IData, IMovie } from "../../types/interfaces";
+import Paginate from "../../components/paginate/Paginate";
 
 const HeroPage: SFC<{ history: IHistory; location: ILocation }> = ({
   history,
   location
 }) => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<IMovie[]>([]);
   const [sort, setSort] = useState("rating");
   const [order, setOrder] = useState("asc");
-  const [page, setPage] = useState(1);
+  const [current_page, setPage] = useState(1);
+  const [previous_page, setPreviousPage] = useState(1);
+  const [next_page, setNextPage] = useState(2);
+  const [count, setCount] = useState(0);
 
   function handleSort(item: string) {
     const newOrder = order === "asc" ? "desc" : "asc";
     setSort(item);
     setOrder(newOrder);
+  }
+
+  function handleClick(page: number) {
+    setPage(page);
   }
 
   useEffect(() => {
@@ -26,17 +34,31 @@ const HeroPage: SFC<{ history: IHistory; location: ILocation }> = ({
       const url = `${path.substring(
         1,
         path.length
-      )}?page=${page}&sort=${sort}&order=${order}`;
-      return Api.get(url).then(({ data: { data } }) => {
-        setMovies(data);
-      });
+      )}?page=${current_page}&sort=${sort}&order=${order}`;
+      return Api.get(url).then(
+        ({ data, current_page, next_page, previous_page, count }) => {
+          console.log(data, current_page, next_page, previous_page, count);
+          setMovies(data);
+          setPage(current_page);
+          setNextPage(next_page);
+          setPreviousPage(previous_page);
+          setCount(count);
+        }
+      );
     }
     fetchAll();
-  }, [sort, page, location, order]);
+  }, [sort, current_page, location, order]);
   return (
     <Fragment>
       <Sort sort={handleSort} />
       <MediaGrid movies={movies} history={history} location={location} />
+      <Paginate
+        count={count}
+        current_page={current_page}
+        next_page={next_page}
+        previous_page={previous_page}
+        onClick={handleClick}
+      />
     </Fragment>
   );
 };
